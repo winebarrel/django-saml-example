0c76c5163e40cfd424544fb65ac2bc28997e1feb
winebarrel <sugawara@winebarrel.jp>
2023-01-22 23:51:59 +0900

    WIP

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('admin/login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path("admin/", admin.site.urls),
     path("", include("blog.urls")),
     url(r"^saml2/", include("djangosaml2.urls")),
